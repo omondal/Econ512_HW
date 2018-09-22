d72facfded405ff43b4cb73265e7426fd7958ecc
Orville Dustin Mondal <orville.dm@gmail.com>
2018-09-22 13:47:33 -0400

    Added a readme for the function

--- HW2/cournot_hw2.m
+++ HW2/cournot_hw2.m
@@ -1,4 +1,7 @@
 function [fval, Jac]=cournot_hw2(p)
+%Given a two dimensional price vector, this returns the FOC with prices
+%subtracted, as fval. This is the appropriate system to solve for an
+%equilibrium. Jac is the Jacobian for this system.
 fval=[(1+exp(2-(1))+exp(2-p(2)))/(1+exp(2-p(2)))-p(1);(1+exp(2-(1))+exp(2-p(2)))/(1+exp(2-p(1)))-p(2)];
 Jac=[(-exp(2-p(1)))/(1+exp(2-p(2)))-1 (exp(4-sum(p)))/(1+exp(2-p(2)))^2;...
       (exp(4-sum(p)))/(1+exp(2-p(1)))^2 (-exp(2-p(2)))/(1+exp(2-p(1)))-1];
