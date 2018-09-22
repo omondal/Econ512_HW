9544df1cb1b1711a8de7678e5a15b87a1acbfb24
Orville Dustin Mondal <orville.dm@gmail.com>
2018-09-22 15:21:01 -0400

    Generalised the function for convenience

--- HW2/cournot_hw2.m
+++ HW2/cournot_hw2.m
@@ -1,8 +1,8 @@
-function [fval, Jac]=cournot_hw2(p)
-%Given a two dimensional price vector, this returns the FOC with prices
+function [fval, Jac]=cournot_hw2(p,v)
+%Given a two dimensional price vector, and valuation vector, this returns the FOC with prices
 %subtracted, as fval. This is the appropriate system to solve for an
-%equilibrium. Jac is the Jacobian for this system.
-fval=[(1+exp(2-(1))+exp(2-p(2)))/(1+exp(2-p(2)))-p(1);(1+exp(2-(1))+exp(2-p(2)))/(1+exp(2-p(1)))-p(2)];
-Jac=[(-exp(2-p(1)))/(1+exp(2-p(2)))-1 (exp(4-sum(p)))/(1+exp(2-p(2)))^2;...
-      (exp(4-sum(p)))/(1+exp(2-p(1)))^2 (-exp(2-p(2)))/(1+exp(2-p(1)))-1];
+%equilibrium. Jac is the Jacobian
+fval=[(1+exp(v(1)-p(1))+exp(v(2)-p(2)))/(1+exp(v(2)-p(2)))-p(1);(1+exp(v(1)-p(1))+exp(v(2)-p(2)))/(1+exp(v(1)-p(1)))-p(2)];
+Jac=[(-exp(v(1)-p(1)))/(1+exp(v(2)-p(2)))-1 (exp(sum(v)-sum(p)))/(1+exp(v(2)-p(2)))^2;...
+      (exp(sum(v)-sum(p)))/(1+exp(v(1)-p(1)))^2 (-exp(v(2)-p(2)))/(1+exp(v(1)-p(1)))-1];
 end
