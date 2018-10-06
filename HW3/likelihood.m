c1e04803c7a041fdafcd87cce98e1d963aaa0fc6
Orville Dustin Mondal <orville.dm@gmail.com>
2018-10-06 18:12:51 -0400

    Likelihood function updated

--- HW3/likelihood.m
+++ HW3/likelihood.m
@@ -1,6 +1,11 @@
-function L=likelihood(y,x,b)
+function [L J]=likelihood(y,x,b)
 %Returns the likelhood function, given a matrix of covariates 'x', and a vector of the dependent variable 'y', assuming a Poisson regression is being
 %estimated. 'b' is the coefficient.
+s=size(x);
+t1=y-exp(x*b);
+t2=kron(t1,ones(1,s(2)));
+z1=t2.*x;
+J=sum(z1,1)'/s(1);
 L1=-exp(x*b)+y.*x*b;
-L=sum(L1);
+L=sum(L1)/s(1);
 end
